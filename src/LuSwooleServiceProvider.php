bea860ad6e5d610253eb47076674f6c1050660d1
刘大为 <304646940@qq.com>
2018-07-04 17:35:32 +0800

    dir up

---
+++ src/LuSwooleServiceProvider.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace blackjue\luswoole;
+
+use Illuminate\Support\ServiceProvider;
+
+class LuSwooleServiceProvider extends ServiceProvider
+{
+
+    public function boot()
+    { 
+         
+    }
+
+    public function register()
+    {
+        
+    }
+
+    public function test(){
+        
+    }
+
+}
